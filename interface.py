c0606676e4f09106a0c1117008d0af3819a97ce0
Diko <diko.prabowo@gmail.com>
2023-05-22 13:36:08 +0700

    edit notebook and interface.py

--- interface.py
+++ interface.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 from agent import agent_query, generate_agent
 
 st.title("HUMANIZE YOUR DATA!")
@@ -17,28 +16,6 @@ if st.button("Submit Query", type="primary"):
     # Query the agent.
     response = agent_query(agent=agent, query=query)
 
-    # Decode the response.
-    #response_dict = json.loads(response)
-
     # Write the response to the Streamlit app.
     st.write(response)
-    # if "answer" in response_dict:
-    #     st.write(response_dict['answer])
-
-    # if "bar" in response_dict:
-    #     data = response_dict["bar"]
-    #     df = pd.DataFrame(data)
-    #     df.set_index("columns", inplace=True)
-    #     st.bar_chart(df)
-
-    # if "line" in response_dict:
-    #     data = response_dict["line"]
-    #     df = pd.DataFrame(data)
-    #     df.set_index("columns", inplace=True)
-    #     st.line_chart(df)
-
-    # if "table" in response_dict:
-    #     data = response_dict["table"]
-    #     df = pd.DataFrame(data["data"], columns=data["columns"])
-    #     st.table(df)
 
